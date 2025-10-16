82e46ec8da4e56f233e663e2ec850ceed540bab3
mhrastegari <mh.rastegari@outlook.com>
2025-10-16 16:42:46 +0330

    add AssemblyInfo

---
+++ src/Maui.ProgressRing/Properties/AssemblyInfo.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://mhrastegari.com/progressring", "Maui.ProgressRing")]
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://mhrastegari.com/progressring", "mhr")]
